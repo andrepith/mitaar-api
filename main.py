deac47063a01da63015cc312ad7634c6b7f20c30
Andre Adikara <andrepith@yahoo.co.uk>
2025-06-16 14:52:13 +0700

    use auth on main file

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from routes.employees import router as employee_router
+from routes.auth import router as auth_router
 
 app = FastAPI()
 
 app.include_router(employee_router)
+app.include_router(auth_router)
